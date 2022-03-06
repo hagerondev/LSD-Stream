2e730fa3f5d0ab7982b6290dfcf79fe1a9e79fda
hageron1229 <hageronprogram@gmail.com>
2022-03-06 20:20:09 +0900

    deploy

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   reactStrictMode: true,
   //distDir: "",
+  generateBuildId: async () => {
+    var S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    var N = 16
+    const b_id = Array.from(Array(N)).map(() => S[Math.floor(Math.random() * S.length)]).join('')
+    console.log("BUILD ID :", b_id)
+    return b_id
+  },
 }
