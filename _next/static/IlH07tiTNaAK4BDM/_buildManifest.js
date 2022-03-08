c1d4de69430696a18083c757766b787fc8d30ed4
hagerondev <hagerondev@users.noreply.github.com>
2022-03-08 19:03:19 +0000

    deploy: bb8c9343ba4ce32cb129027087d79746afa63383

--- _next/static/IlH07tiTNaAK4BDM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-247c0131e9afb2b7.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/live":["static/chunks/pages/live-2af678524a655484.js"],sortedPages:["/","/_app","/_error","/live"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
