058e925c6aa8a15a93622050d133cc6fe5ded9c1
hagerondev <hageronprogram@gmail.com>
2022-03-14 19:30:09 +0900

    delete unuse

---
+++ client/public/ws.js
@@ -0,0 +1,39 @@
+
+function ws_connect(button_id, callback) {
+    //WebSocket接続
+    //var connection = new WebSocket("wss://lsd-com.hageron.com:443/");
+    var connection = new WebSocket("wss://lsd-com.hageron.com:9001/");
+    console.log("connecting")
+    //接続通知
+    connection.onopen = function (event) {
+        //document.getElementById("eventType").value = "通信接続イベント受信";
+        //document.getElementById("dispMsg").value = event.data;
+        console.log("connected")
+    };
+
+    //エラー発生
+    connection.onerror = function (error) {
+        console.log(error)
+        //document.getElementById("eventType").value = "エラー発生イベント受信";
+        //document.getElementById("dispMsg").value = error.data;
+        console.log("ERROR", error);
+    };
+
+    //メッセージ受信
+    connection.onmessage = function (event) {
+        //document.getElementById("eventType").value = "メッセージ受信";
+        //document.getElementById("dispMsg").value = event.data;
+        //console.log(event.data);
+        callback(event)
+    };
+
+    //切断
+    connection.onclose = function () {
+        //document.getElementById("eventType").value = "通信切断イベント受信";
+        //document.getElementById("dispMsg").value = "";
+    };
+
+    document.getElementById(button_id).addEventListener("click", function () {
+        connection.send("good");
+    });
+}
